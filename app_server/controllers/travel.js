3b157bff31a2220181df6a728be475ca93e02145
Danielle Eeg <daniellegeeg@gmail.com>
2025-05-02 12:12:22 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,14 @@
- /* GET travel view */
+/* GET travel view */
+var fs = require('fs');
 
- const travel = (req, res) => {
-    res.render('travel', {title:'Travlr Getaways'});
- };
+// Read trip data from json file
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
- module.exports = {
+
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways', trips });
+};
+
+module.exports = {
     travel
- }
+}
